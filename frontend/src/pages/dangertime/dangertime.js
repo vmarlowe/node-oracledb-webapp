f8061cd86afc2958b7d484197c52642ca571e6b0
taikantomita <taikantomita@gmail.com>
2022-12-06 17:18:00 -0500

    changed DangerTime to arrow function

--- frontend/src/pages/dangertime/dangertime.js
+++ frontend/src/pages/dangertime/dangertime.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
-function DangerTime() {
+/*
+For demonstrating complex query:
+6.2 Which zones (residential, commercial, or public) had the highest percentage increase in complaints at night time compared to day time?
+*/
+
+const DangerTime =()=> {
   return (
     <div className='dt' id='dt1'>
     <div>these are dangerous time we live in - taikan</div>
