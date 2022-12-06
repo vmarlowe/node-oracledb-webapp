280ba5d4605a6f9427f8956fa097b74cfd1e99bf
taikantomita <taikantomita@gmail.com>
2022-12-06 15:50:23 -0500

    Cleaned up unused stuff

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,53 +2,6 @@ import React from "react";
 import ReactDom from 'react-dom/client';
 import "../src/main.scss";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-
-
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  BrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
-
-
-import Querytest from "./pages/Querytest";
-import ErrorPage from "./pages/error-page";
-import Trends from "./pages/trends/trends";
-import Dashboard from "./pages/dashboard/dashboard";
-
-/*
-const router = (
-  <BrowserRouter>
-    <Route exact path="/" element={Root} />
-    <Route path="/querytester" component={Querytest} />
-  </BrowserRouter>
-);
-*/
-
-/* const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Dashboard />
-  },
-  {
-    path: "/querytester",
-    element: <Querytest />
-  },
-  
-]); */
-/*
-ReactDom.render (
-<React.StrictMode>
-  <App />
-</React.StrictMode>
-, document.getElementById("root")
-
-);
-*/
 
 ReactDom.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
